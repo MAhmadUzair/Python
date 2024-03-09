0f57a6a7bb7445657de4579484a53c01bb149965
Uzair <mauzair19@gmail.com>
2024-03-10 00:11:13 +0500

    Strings

---
+++ Python Data Types/Strings.py
@@ -0,0 +1,20 @@
+# Python Program for
+# Creation of String
+
+# Creating a String
+# with single Quotes
+String1 = 'Welcome to the Uzair World'
+print("String with the use of Single Quotes: ")
+print(String1)
+
+# Creating a String
+# with double Quotes
+String1 = "I'm a Data Scientist"
+print("\nString with the use of Double Quotes: ")
+print(String1)
+
+# Creating a String
+# with triple Quotes
+String1 = '''I'm a Geek and I live in a world of "Geeks"'''
+print("\nString with the use of Triple Quotes: ")
+print(String1)
